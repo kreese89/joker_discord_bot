0a791d2f97137133dea192e1e96fb27207e98515
Kyle Reese <kylereese@kreesemac.local>
2021-12-20 15:55:50 -0500

    set up discordjs and joker quotes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import discord from 'discord.js';
+require('dotenv').config();
+const Discord = require('discord.js');
+const sample = require('lodash.sample');
+const quotes = require('./quotes.js');
+
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on('messageCreate', (msg) => {
+  if (msg.content == '!quote') {
+    msg.channel.send('"' + sample(quotes) + '" - The Joker');
+  }
+});
+
+client.login(process.env.TOKEN);
